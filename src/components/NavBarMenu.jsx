f86d7f30c4f3605089d9b61be8f8e4bda9a60610
darekgrados <dariuszgrados@gmail.com>
2024-10-30 22:45:23 +0100

    lab2 - zadanie 4

--- src/components/NavBarMenu.jsx
+++ src/components/NavBarMenu.jsx
@@ -1,26 +1,5 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-
-// const NavBarMenu = ({ items }) => {
-//   return (
-//     <Navbar expand="lg" className="bg-body-tertiary">
-//       <Container>
-//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             {items.map((item) => (
-//               <LinkContainer key={item.id} to={`/${item.label.toLowerCase().replace(" ", "")}`}>
-//                 <Nav.Link>{item.label}</Nav.Link>
-//               </LinkContainer>
-//             ))}
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
 
 const NavBarMenu = ({ items }) => {
   return (
@@ -31,7 +10,7 @@ const NavBarMenu = ({ items }) => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {items.map((item) => (
-              <Nav.Link href={item.url} key={item.id}>
+              <Nav.Link key={item.id} href={`/${item.label.toLowerCase().replace(" ", "")}`}>
                 {item.label}
               </Nav.Link>
             ))}
@@ -41,5 +20,25 @@ const NavBarMenu = ({ items }) => {
     </Navbar>
   );
 };
+
+// const NavBarMenu = ({ items }) => {
+//   return (
+//     <Navbar expand="lg" className="bg-body-tertiary">
+//       <Container>
+//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             {items.map((item) => (
+//               <Nav.Link href={item.url} key={item.id}>
+//                 {item.label}
+//               </Nav.Link>
+//             ))}
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// };
 
 export default NavBarMenu;
--- src/pages/Lab2.jsx
+++ src/pages/Lab2.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { data } from '../data/module-data'; 
 
 function Lab2() {
   const { id } = useParams();
+  const person = data.find((p) => p.id === Number(id));
 
   return (
     <>
-      <h1>Laboratorium 2</h1>
-      <p>Identyfikator osoby: {id}</p>
+      <h1>Laboratorium 2 - Profil osoby</h1>
+      {id ? (
+        person ? (
+          <div>
+            <p>Id: {person.id}</p>
+            <p>Imię: {person.name}</p>
+            <p>Data urodzenia: {person.birth}</p>
+            <p>Kolor oczu: {person.eyes}</p>
+          </div>
+        ) : (
+          <span>Nie znaleziono osoby o tym identyfikatorze.</span>
+        )
+      ) : (
+        <span>Brak identyfikatora osoby.</span>
+      )}
     </>
   );
 }
